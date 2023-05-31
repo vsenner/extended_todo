78af044938094774593a835d4264f83cc4918aa3
nikitayunak <nikita.yunak25@gmail.com>
2023-05-31 15:18:47 +0300

    cors fix

--- server/index.go
+++ server/index.go
@@ -10,12 +10,31 @@ import (
 	"net/http"
 
 	"github.com/dgrijalva/jwt-go"
+	//"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", c.Request.Header.Get("Origin"))
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func Server() {
 	router := gin.Default()
+
+	router.Use(CORSMiddleware())
 
 	router.POST("/user/registration", user_unauthorized_controller.Registration)
 	router.POST("/user/login", user_unauthorized_controller.Login)
